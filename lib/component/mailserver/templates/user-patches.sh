8688215123c1026688ce54803f67c8cbcf11bb69
Robert Schumann <robert@schumann.link>
2022-09-11 19:09:19 +0200

    update opendkim settings

--- lib/component/mailserver/templates/user-patches.sh
+++ lib/component/mailserver/templates/user-patches.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # replace opendkim config keys with ldap queries
-sed -i "s%^SigningTable.*$%SigningTable           ldap://${LDAP_SERVER_HOST}/${LDAP_SEARCH_BASE}?DKIMSelector?sub?(DKIMIdentity=@\$d)%" /etc/opendkim.conf
-sed -i "s%^KeyTable.*$%KeyTable               ldap://${LDAP_SERVER_HOST}/${LDAP_SEARCH_BASE}?DKIMDomain,DKIMSelector,DKIMKey,?sub?(DKIMSelector=\$s)%" /etc/opendkim.conf
+sed -i "s%^SigningTable.*$%SigningTable            ldap://${LDAP_SERVER_HOST}/${LDAP_SEARCH_BASE}?DKIMSelector?sub?(DKIMIdentity=@\$d)%" /etc/opendkim.conf
+sed -i "s%^KeyTable.*$%KeyTable                ldap://${LDAP_SERVER_HOST}/${LDAP_SEARCH_BASE}?DKIMDomain,DKIMSelector,DKIMKey,?sub?(DKIMSelector=\$d)%" /etc/opendkim.conf
 echo "LDAPBindUser            ${LDAP_BIND_DN}" >> /etc/opendkim.conf
 echo "LDAPBindPassword        ${LDAP_BIND_PW}" >> /etc/opendkim.conf
 echo "LDAPUseTLS              true" >> /etc/opendkim.conf
